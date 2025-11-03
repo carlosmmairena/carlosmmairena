e7633e3467e39b373902574bfba01603ddaeeb36
Carlos Mairena <carlosmiguelmairena@gmail.com>
2025-11-03 16:11:05 -0600

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://carlosmmairena.com',
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
